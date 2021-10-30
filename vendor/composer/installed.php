3b5b7d36ff374e3367856a6ba93d7035971e4a3d
Sergio Danilo Jr <sergiodanilojr@hotmail.com>
2021-10-29 21:17:38 -0300

    Support PHP 8 Version

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4cd3534bb284ab22096518656af7d4a1ee23ab5a',
+        'name' => 'elephpant/breadcrumb',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elephpant/breadcrumb' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4cd3534bb284ab22096518656af7d4a1ee23ab5a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
